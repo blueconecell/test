ad377ebf32489608d9f12a64ccf5affb8b19b84a
blueconecell <kimjeongyeon113@gmail.com>
2020-01-17 11:09:37 +0900

    ddddddd_fixed

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        ghjkghjk
+        dddddddddddddddddddddd
       </v-toolbar-title>
       <v-spacer></v-spacer>
     </v-toolbar>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>
-    example;;;
+ <div>
+    <span v-if="seen">
+      00000000000000000
+      abcd {{ seen }}
+
+    </span>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      seen: true,
+      message: 'qwer'
+    }
+  }
+}
+</script>
