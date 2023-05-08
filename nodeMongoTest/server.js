88d4e73498ff8f65f717b400f2e4923ab4981b8b
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2023-05-09 00:31:12 +0900

    can

--- nodeMongoTest/server.js
+++ nodeMongoTest/server.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 app.listen(3000, function () {
   console.log("listening on 3000");
+});
+app.get("/", function (요청, 응답) {
+  응답.sendFile(__dirname + "/index.html");
+});
+app.get("/write", function (요청, 응답) {
+  응답.sendFile(__dirname + "/write.html");
+});
+app.post("/add", function (요청, 응답) {
+  console.log(요청.body);
+  응답.send("전송완료");
 });
